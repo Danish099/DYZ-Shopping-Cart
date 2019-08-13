88ae45b8466dd29aeaa0b0913f40a1ce68c08c51
Danish099 <danishjawaid099@gmail.com>
2019-08-13 07:39:46 -0700

    Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Javascript Shopping Cart Project With Firebase Authorization
+
+Features:
+
+1) Login With Authorization
+2) Add Items to the Cart
+3) Delete Items from the Cart
+4) Logout
+
+Features to be added in future:
+
+1) Search bar functionality
+2) Checkout
---
+++ js/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAACBmIRvrnrHLBPPcZmNJfFwzTVp8yVD8",
+  authDomain: "loginform-ed2c1.firebaseapp.com",
+  databaseURL: "https://loginform-ed2c1.firebaseio.com",
+  projectId: "loginform-ed2c1",
+  storageBucket: "",
+  messagingSenderId: "238593361393",
+  appId: "1:238593361393:web:c4a070420f166784"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
